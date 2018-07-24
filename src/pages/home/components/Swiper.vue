1b01aa79101e526a53d895ee4858619b6275e400
Lio5n <18651852362@163.com>
2018-07-25 01:00:02 +0800

    add swiper

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,13 +1,50 @@
 <template>
-
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <!-- slides -->
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl"/>
+            </swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination" slot="pagination"></div>
+            <!--<div class="swiper-button-prev" slot="button-prev"></div>-->
+            <!--<div class="swiper-button-next" slot="button-next"></div>-->
+            <!--<div class="swiper-scrollbar"   slot="scrollbar"></div>-->
+        </swiper>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "HomeSwiper"
+        name: "HomeSwiper",
+        data() {
+            return {
+                swiperOption: {
+                    pagination: '.swiper-pagination',
+                    loop: true
+                },
+                swiperList: [{
+                    id: '0001',
+                    imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg'
+                }, {
+                    id: '0002',
+                    imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/anquan_20180716.png'
+                }]
+            }
+        }
     }
 </script>
 
 <style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background #fff
 
+    .wrapper
+        overflow hidden
+        width 100%
+        height 0
+        padding-bottom 24.5%
+        background #eee
+        .swiper-img
+            width 100%
 </style>
